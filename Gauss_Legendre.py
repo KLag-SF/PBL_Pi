7f9a641ababdb6c340f1b8b21e8320bb304fd3a8
KLag-SF <k.lagopus.sf19@gmail.com>
2020-11-05 13:12:17 +0900

    基本的なガウス・ルジャンドル法の実装

--- Gauss_Legendre.py
+++ Gauss_Legendre.py
@@ -1,7 +1,31 @@
 import math
 from decimal import Decimal
 
-a = 1
-b = Decimal('0.5') * Decimal('2') ** Decimal('0.5')
-t = Decimal('0.25')
-p = 0
+def gauss(a, b, t, p):
+    new_a = (a + b) / 2
+    new_b = (a * b) ** Decimal('0.5')
+    new_t = t - p * ((a - new_a) ** 2)
+    new_p = 2 * p
+    return (new_a, new_b, new_t, new_p)
+
+def calc(a, b, t, p):
+    return ((a + b) ** 2) / (4 * t)
+
+def main():
+    a = 1
+    b = Decimal('0.5') * Decimal('2') ** Decimal('0.5')
+    t = Decimal('0.25')
+    p = 1
+
+    nums = (a, b, t, p)
+
+    loop = int(input("Input number of times to calculate. > "))
+
+    count = 0
+    
+    for i in range(loop):
+        print(str(count)+ ":" + str(calc(*nums)))
+        nums = gauss(*nums)
+        count += 1
+    
+main()

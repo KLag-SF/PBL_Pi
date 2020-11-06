64f2c7072d513306552978fe1d25819c8e1a14ee
KLag-SF <k.lagopus.sf19@gmail.com>
2020-11-06 12:38:52 +0900

    改行・空白を除いて比較するように改良

--- CheckDigit.py
+++ CheckDigit.py
@@ -5,35 +5,16 @@ skip_letter = [" ", "\n"]
 i = 0
 
 with open(path1, 'r') as f1, open(path2, 'r') as f2:
-    s1 = f1.read()
-    s2 = f2.read()
+    s1 = f1.read().replace(' ', '').replace('\n', '')
+    s2 = f2.read().replace(' ', '').replace('\n', '')
 
-    """for i in range(len(s1)):
+    for i in range(len(s1)):
         try:
-            if (s1[i] in skip_letter) or (s2[i] in skip_letter):
-                continue
-            elif s1[i] != s2[i]:
+            if s1[i] != s2[i]:
                 print(s1[i - 5 : i + 5])
                 print(s2[i - 5 : i + 5])
-                print(str(i) + "digits correct.")
+                print(str(i) + " digits correct.")
                 break
         except IndexError:
-            print(str(i) + "digits correct.")
+            print(str(i) + " digits correct.")
             break
-    """
-
-    while(s1[i] == s2[i]):
-        try:
-            if (s1[i] in skip_letter) or (s2[i] in skip_letter):
-                pass
-            else:
-                i += 1
-        except IndexError:
-            print("Reached EOF.")
-            print(str(i) + "digits correct.")
-            break
-    else:
-        print(s1[i - 5 : i + 5])
-        print(s2[i - 5 : i + 5])
-        print(str(i) + "digits correct.")
-    
